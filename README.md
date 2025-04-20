9696419cf81b66682e0f06f4614b1446aea23d64
tmgoncalves <tmgoncalves@hotmail.com>
2025-04-19 21:04:57 -0300

    construcao inicial do layout

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#po-transfer
+# po-transfer
 
 Esse repositório tem como objetivo explorar o framework de Front-end PO-UI.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+import { PoDynamicFormField, PoMenuItem } from '@po-ui/ng-components';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent {
-  title = 'transfer';
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: () => alert('Hello World!') }
+  ];
+
+  propertyForm: Array<PoDynamicFormField> = [
+    {property: 'sender', label: 'Remetente', placeholder: 'Remetente', required: true, gridColumns: 4},
+    {property: 'recipient', label: 'Destinatário', placeholder: 'Destinatário', required: true, gridColumns: 4},
+    {property: 'money', label: 'Valor', type: 'currency', placeholder: 'Valor', required: true, gridColumns: 4},
+    {property: 'description', label: 'Descrição', required: true, gridColumns: 12, rows: 5, placeholder: 'Descrição'}
+  ]
+
+  save() {
+    alert('Ok');
+  }
 }
